a5209c9de3c38a2325bd11e1fa84f54a44022ff2
Josue Reyes <reyesnatanael28@gmail.com>
2024-11-13 15:39:15 -0500

    Se actualizaron las tablas, se creo nueva tabla TIPO_VISITANTES

---
+++ Scripts/DDL/SELECT/01_initial-queries.sql
@@ -0,0 +1,6 @@
+-- 1. Mostrar todos los animales que viven en el habitad santuario de aves tropicales
+SELECT *
+FROM animales
+WHERE habitat = 'Santuario de Aves';
+
+--2. Mostrar el nombre del cuidador, con el alias cuidador, ordenados alfabeticamente
---
+++ Scripts/DML/load_create_tables.bat
@@ -0,0 +1,23 @@
+@echo off
+chcp 65001
+setlocal
+
+:: Configura las variables de conexión
+set "PGHOST=localhost"
+set "PGPORT=5432"
+set "PGUSER=adminzoo"
+set "PGPASSWORD=josuereyes"
+set "PGDATABASE=zoologico"
+
+:: Ejecuta cada archivo SQL en orden
+
+echo Ejecutando TableDefinitions.sql...
+psql -h %PGHOST% -p %PGPORT% -U %PGUSER% -d %PGDATABASE% -f "create_tables.sql"
+if %errorlevel% neq 0 (
+    echo Error ejecutando TableDefinitions.sql
+    exit /b %errorlevel%
+)
+
+echo Script ejecutado correctamente.
+endlocal
+pause
